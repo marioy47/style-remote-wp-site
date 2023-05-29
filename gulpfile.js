8630e3535f32feb413fc707884b85654fe6a7848
Mario Yepes <marioy47@gmail.com>
2023-05-28 23:13:50 -0500

    Adding postcss to gulp for better css output

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 // gulpfile.js
-const { src, dest, watch } = require( 'gulp' );
-const webpackStream = require( 'webpack-stream' );
-const wpWebpackConfig = require( '@wordpress/scripts/config/webpack.config' );
+const { src, dest, watch, series } = require( 'gulp' );
 const path = require( 'path' );
-const fs = require( 'fs' );
+const webpackStream = require( 'webpack-stream' );
 const sass = require( 'gulp-sass' )( require( 'sass' ) );
 const bs = require( 'browser-sync' );
+const autoprefixer = require( 'autoprefixer' );
+const postcss = require( 'gulp-postcss' );
+const prettier = require( 'gulp-prettier' );
 
 // Modify WordPress webpack. Change the output dir to dist/ and the name of the start file
+const wpWebpackConfig = require( '@wordpress/scripts/config/webpack.config' );
 const localWebpackConfig = {
 	...wpWebpackConfig,
 	entry: {
@@ -19,18 +21,29 @@ const localWebpackConfig = {
 	},
 };
 
-const bundle = () => {
+/**
+ * Bundle JavaScript files into a single scripts.js file
+ */
+const jsBundle = () => {
 	return src( './js/scripts.js' )
 		.pipe( webpackStream( localWebpackConfig ) ) // Use WordPress webpack
 		.pipe( dest( './dist/' ) );
 };
 
+/**
+ * Compile SCSS into a single styles.css file
+ */
 const scss = () => {
 	return src( './scss/**/*.scss' )
 		.pipe( sass().on( 'error', sass.logError ) )
+		.pipe( postcss( [ autoprefixer( { browserlist: [ '> 1%' ] } ) ] ) )
+		.pipe( prettier() )
 		.pipe( dest( './dist/css' ) );
 };
 
+/**
+ * Apply bundle() or scss() when a file changes.
+ */
 const look = () => {
 	bs.init( {
 		proxy: process.env.REMOTE_HOST || 'https://archive.org',
@@ -51,9 +64,11 @@ const look = () => {
 	} );
 
 	watch( './scss/**/*.scss', scss );
-	watch( './js/**/*.js', bundle );
+	watch( './js/**/*.js', jsBundle );
 };
 
-exports.bundle = bundle;
+// Export function so gulp can execute them.
+exports.bundle = jsBundle;
 exports.scss = scss;
 exports.watch = look;
+exports.build = series( jsBundle, scss );
