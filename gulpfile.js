dcd9017d31da42a52ab0d28461a45bf22e3b3da5
Mario Yepes <marioy47@gmail.com>
2023-05-28 22:47:36 -0500

    Adding BrowserSync to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ const { src, dest, watch } = require( 'gulp' );
 const webpackStream = require( 'webpack-stream' );
 const wpWebpackConfig = require( '@wordpress/scripts/config/webpack.config' );
 const path = require( 'path' );
+const fs = require( 'fs' );
 const sass = require( 'gulp-sass' )( require( 'sass' ) );
+const bs = require( 'browser-sync' );
 
 // Modify WordPress webpack. Change the output dir to dist/ and the name of the start file
 const localWebpackConfig = {
@@ -30,6 +32,24 @@ const scss = () => {
 };
 
 const look = () => {
+	bs.init( {
+		proxy: process.env.REMOTE_HOST || 'https://archive.org',
+		serveStatic: [ './' ],
+		files: [ 'dist/css/*.css', 'dist/js/*.js' ],
+		rewriteRules: [
+			{
+				match: /<\/head>/i,
+				fn: () =>
+					'<link rel="stylesheet" type="text/css" href="/dist/css/styles.css"></head>',
+			},
+			{
+				match: /<\/body>/i,
+				fn: () =>
+					'<script type="text/javascript" src="/dist/js/scripts.js"></script>',
+			},
+		],
+	} );
+
 	watch( './scss/**/*.scss', scss );
 	watch( './js/**/*.js', bundle );
 };
