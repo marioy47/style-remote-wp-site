b916735201d1a0fdd18bbb8aea90ddddeb9f4fe7
Mario Yepes <marioy47@gmail.com>
2023-05-28 22:16:04 -0500

    Adding scss compilation to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
 // gulpfile.js
-const { src, dest } = require( 'gulp' );
+const { src, dest, watch } = require( 'gulp' );
 const webpackStream = require( 'webpack-stream' );
 const wpWebpackConfig = require( '@wordpress/scripts/config/webpack.config' );
 const path = require( 'path' );
+const sass = require( 'gulp-sass' )( require( 'sass' ) );
 
 // Modify WordPress webpack. Change the output dir to dist/ and the name of the start file
 const localWebpackConfig = {
@@ -22,4 +23,17 @@ const bundle = () => {
 		.pipe( dest( './dist/' ) );
 };
 
+const scss = () => {
+	return src( './scss/**/*.scss' )
+		.pipe( sass().on( 'error', sass.logError ) )
+		.pipe( dest( './dist/css' ) );
+};
+
+const look = () => {
+	watch( './scss/**/*.scss', scss );
+	watch( './js/**/*.js', bundle );
+};
+
 exports.bundle = bundle;
+exports.scss = scss;
+exports.watch = look;
--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,5 @@
 // This is the starting poing of all your javascript. Since we're using
 // Webpack, you can use import() and require().
+
+const sum3 = require( './sum3' );
+sum3( 5 );
