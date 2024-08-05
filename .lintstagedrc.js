d00cd093071b3ac4aa357563aa9bc12b6ff6f3ef
Mario Yepes <marioy47@gmail.com>
2024-08-05 14:16:55 -0500

    fix: disable cSpell since most projects use propietary words

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,5 +2,5 @@ export default {
 	'*.scss': 'npm run format:css',
 	'*.js': 'npm run format:js',
 	'*.md': 'npm run format:md',
-	'*': "npx cspell" // Only cspell on changed files.
+	// '*': "npx cspell" // Only cspell on changed files.
 };
